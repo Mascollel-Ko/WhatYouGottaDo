7b49d062a4a4b81bfc3cdcc50692923fa63231c0
Mascollel-Ko <clakry123@gmail.com>
2026-06-30 08:27:54 +0900

    chore(release): bump v0.3.9.1 smash speed records

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.whatyougottatrain.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 309000
-        versionName = "v0.3.9.0"
+        versionCode = 309001
+        versionName = "v0.3.9.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
